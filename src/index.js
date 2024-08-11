4823c07cbb0830d15fdb426b20ad5a363dba0690
KZhukow <kosty.zhukov.2011@gmail.com>
2024-08-11 14:47:05 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return matrix ? matrix.map((it, i) => i % 2 ? it.sort((a, b) => b - a) : it.sort((a, b) => a - b)).flat() : [];
 }
